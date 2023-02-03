549f7cd03d1b59eb5eacf94b1a2e002af303d9f0
cramoscr <cramoscr@gmail.com>
2023-02-03 16:21:07 -0600

    Initial commit of the Keeper-App

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// index.js
+// --------
+// Updated: cramos 03/feb/2023
+//
+// Keeper-App is a ReactJS based app.
+// Inspired in: [ TheLondonAppBrewery course ]
+//
+// For running locally, enter
+//   npm install  [ install nodeJS + prerequisits locally ]
+//   npm start    [ run the app locally ]
+//
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
