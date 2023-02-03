aeecd74ac248518d83d50df9996644cd035596f4
cramoscr <cramoscr@gmail.com>
2023-02-03 16:42:15 -0600

    Producing the production version [ build folder ]

---
+++ build/precache-manifest.3c90494481f45a3e3e497d03133b1a94.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce4ece76e895467bd3eb",
+    "url": "/static/js/main.ce4ece76.chunk.js"
+  },
+  {
+    "revision": "7f2bd4ae15300fb9ed34",
+    "url": "/static/js/1.7f2bd4ae.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "62396dd26dea1e5c62188fb3d90c6bc2",
+    "url": "/index.html"
+  }
+];
